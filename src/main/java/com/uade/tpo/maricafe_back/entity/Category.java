88db79a2b3f600707d5eebef9c426d80680ae588
Romina Micaela Palomino <ropalomino@uade.edu.ar>
2025-08-23 20:31:40 -0300

    initial commit mas codebase profe

---
+++ src/main/java/com/uade/tpo/maricafe_back/entity/Category.java
@@ -0,0 +1,19 @@
+package com.uade.tpo.maricafe_back.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String description;
+}
---
+++ src/main/java/com/uade/tpo/maricafe_back/entity/dto/CategoryRequest.java
@@ -0,0 +1,9 @@
+package com.uade.tpo.maricafe_back.entity.dto;
+
+import lombok.Data;
+
+@Data
+public class CategoryRequest {
+    private int id;
+    private String description;
+}
