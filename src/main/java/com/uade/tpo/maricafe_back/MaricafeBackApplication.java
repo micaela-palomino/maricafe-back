0b3cc202698e82c3aafe447601068333ab7019b4
Romina Micaela Palomino <ropalomino@uade.edu.ar>
2025-08-23 19:12:36 -0300

    initial commit

---
+++ src/main/java/com/uade/tpo/maricafe_back/MaricafeBackApplication.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.maricafe_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaricafeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaricafeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uade/tpo/maricafe_back/MaricafeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.tpo.maricafe_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaricafeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
