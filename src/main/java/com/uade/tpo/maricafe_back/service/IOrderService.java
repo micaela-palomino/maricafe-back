ee6993c43e7b0a78a68d184e8039768d62014ed2
UriRefactored <urieluanin@gmail.com>
2025-09-03 00:43:21 -0300

    Endpoints Order

--- src/main/java/com/uade/tpo/maricafe_back/service/IOrderService.java
+++ src/main/java/com/uade/tpo/maricafe_back/service/IOrderService.java
@@ -1,4 +1,10 @@
 package com.uade.tpo.maricafe_back.service;
 
+import com.uade.tpo.maricafe_back.entity.Order;
+import java.util.List;
+
 public interface IOrderService {
+    Order create(Order order);
+    List<Order> findMine();
+    Order findMyOrderById(Integer id);
 }
