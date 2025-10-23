5e3aa36822e058070722080eabcc11146a1bfe95
Gabriel Aran <gabitoaran@gmail.com>
2025-10-23 01:40:10 -0300

    Orders in admin finished

--- src/main/java/com/uade/tpo/maricafe_back/entity/dto/OrderDTO.java
+++ src/main/java/com/uade/tpo/maricafe_back/entity/dto/OrderDTO.java
@@ -3,7 +3,7 @@ package com.uade.tpo.maricafe_back.entity.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
@@ -16,7 +16,7 @@ public class OrderDTO {
     private Integer orderId;
 
     @JsonProperty("order_date")
-    private LocalDate orderDate;
+    private LocalDateTime orderDate;
 
     @JsonProperty("total_price")
     private double totalPrice;
